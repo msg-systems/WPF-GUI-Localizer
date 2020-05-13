6c8b350b563c2a587fab11d7d1f5b0795a310571
thomam <thomam@MSGN11151>
2020-05-13 17:39:49 +0200

    Added Tests for ResourcesTextConverter

--- WPF-GUI-Localizer/InternationalizationTests/Converter/ResourcesTextConverterTests.cs
+++ WPF-GUI-Localizer/InternationalizationTests/Converter/ResourcesTextConverterTests.cs
@@ -1,20 +1,150 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Internationalization.Converter;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Internationalization.Enum;
+using Internationalization.LiteralProvider.Abstract;
+using Internationalization.LiteralProvider.Interface;
+using Internationalization.LiteralProvider.Resource;
+using Internationalization.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Internationalization.Converter.Tests
 {
     [TestClass()]
     public class ResourcesTextConverterTests
     {
-        [TestMethod()]
-        public void ConvertTest()
+        private abstract class MoqLiteralProviderSetupHelper : AbstractLiteralProvider
         {
-            Assert.Fail();
+            public static void Init(ILiteralProvider literalProvider)
+            {
+                Instance = literalProvider;
+            }
+        }
+
+        [TestMethod()]
+        public void Convert_Null_ReturnsEmptyString()
+        {
+            //Arrange
+            var mockLP = new Mock<ILiteralProvider>();
+            MoqLiteralProviderSetupHelper.Init(mockLP.Object);
+            var converter = new ResourcesTextConverter();
+            string resourceKey = null;
+
+            //Act
+            object converted = converter.Convert(
+                resourceKey, typeof(string), null, CultureInfo.InvariantCulture);
+            var convertedAsTargetType = converted as string;
+
+            //Assert
+            Assert.IsInstanceOfType(converted, typeof(string));
+            Assert.AreEqual(string.Empty, convertedAsTargetType);
+        }
+
+        [TestMethod()]
+        public void Convert_InappropriateType_ReturnsEmptyString()
+        {
+            //Arrange
+            var resourceKey = new List<string> {"someKey"};
+            var mockLP = new Mock<ResourceLiteralProvider> { CallBase = true };
+            MoqLiteralProviderSetupHelper.Init(mockLP.Object);
+            var converter = new ResourcesTextConverter();
+
+            //Act
+            object converted = converter.Convert(
+                resourceKey, typeof(string), null, CultureInfo.InvariantCulture);
+            var convertedAsTargetType = converted as string;
+
+            //Assert
+            Assert.IsInstanceOfType(converted, typeof(string));
+            Assert.AreEqual(string.Empty, convertedAsTargetType);
+        }
+
+        [TestMethod()]
+        public void Convert_EmptyString_ReturnsCorresponding()
+        {
+            //Arrange
+            string resourceKey = "";
+            string expectedTranslation = "Hello World!";
+            var mockLP = new Mock<ResourceLiteralProvider> { CallBase = true };
+            mockLP.Setup(resLP => resLP.GetGuiTranslationOfCurrentCulture(resourceKey))
+                .Returns(expectedTranslation);
+            MoqLiteralProviderSetupHelper.Init(mockLP.Object);
+            var converter = new ResourcesTextConverter();
+
+            //Act
+            object converted = converter.Convert(
+                resourceKey, typeof(string), null, CultureInfo.InvariantCulture);
+            var convertedAsTargetType = converted as string;
+
+            //Assert
+            Assert.IsInstanceOfType(converted, typeof(string));
+            Assert.AreEqual(expectedTranslation, convertedAsTargetType);
+        }
+
+        [TestMethod()]
+        public void Convert_ExistingEntry_ReturnsCorresponding()
+        {
+            //Arrange
+            string resourceKey = "someKey";
+            string expectedTranslation = "Hello World!";
+            var mockLP = new Mock<ResourceLiteralProvider>{ CallBase = true };
+            mockLP.Setup(resLP => resLP.GetGuiTranslationOfCurrentCulture(resourceKey))
+                .Returns(expectedTranslation);
+            MoqLiteralProviderSetupHelper.Init(mockLP.Object);
+            var converter = new ResourcesTextConverter();
+
+            //Act
+            object converted = converter.Convert(
+                resourceKey, typeof(string), null, CultureInfo.InvariantCulture);
+            var convertedAsTargetType = converted as string;
+
+            //Assert
+            Assert.IsInstanceOfType(converted, typeof(string));
+            Assert.AreEqual(expectedTranslation, convertedAsTargetType);
+        }
+
+        [TestMethod()]
+        public void Convert_NonExistingEntry_ReturnsEmptyString()
+        {
+            //Arrange
+            string resourceKey = "someKey";
+            var mockLP = new Mock<ResourceLiteralProvider> { CallBase = true };
+            mockLP.Setup(resLP => resLP.GetGuiTranslationOfCurrentCulture(resourceKey))
+                .Returns((string) null);
+            MoqLiteralProviderSetupHelper.Init(mockLP.Object);
+            var converter = new ResourcesTextConverter();
+
+            //Act
+            object converted = converter.Convert(
+                resourceKey, typeof(string), null, CultureInfo.InvariantCulture);
+            var convertedAsTargetType = converted as string;
+
+            //Assert
+            Assert.IsInstanceOfType(converted, typeof(string));
+            Assert.AreEqual(string.Empty, convertedAsTargetType);
+        }
+
+        /// <summary>
+        /// This test assumes not setting <see cref="AbstractLiteralProvider.Instance"/> does not cause endless loops!
+        /// </summary>
+        [TestMethod()]
+        public void Convert_NoLiteralProvider_ReturnsEmptyString()
+        {
+            //Arrange
+            string resourceKey = "someKey";
+            var converter = new ResourcesTextConverter();
+
+            //Act
+            object converted = converter.Convert(
+                resourceKey, typeof(string), null, CultureInfo.InvariantCulture);
+            var convertedAsTargetType = converted as string;
+
+            //Assert
+            Assert.IsInstanceOfType(converted, typeof(string));
+            Assert.AreEqual(string.Empty, convertedAsTargetType);
         }
     }
 }
